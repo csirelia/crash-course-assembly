e868873993838e4e60c67ebf6703e2e3f595a3f9
Bowen <csbowen@gmx.com>
2018-06-27 20:25:13 +0800

    Initial commit

---
+++ first.asm
@@ -0,0 +1,11 @@
+global _main
+
+_main:
+	mov eax, 0
+	ret
+
+;two steps for run .asm file
+;nasm -f macho first.asm -o first.o
+;gcc -m32 first.o -o first
+;./first
+;that is all for run first.asm 
